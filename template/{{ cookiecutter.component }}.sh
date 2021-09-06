99c7a19583451754dbb55ee5da88024b5606026b
Sahil Gulati <sahil.gulati1991@outlook.com>
2021-09-06 15:57:56 +0530

    Update {{ cookiecutter.component }}.sh

--- template/{{ cookiecutter.component }}.sh
+++ template/{{ cookiecutter.component }}.sh
@@ -1,4 +1,4 @@
-echo "Component name is {{ cookiecutter.component }}"
+echo "Component name is {{ cookiecutter.name }}"
 {%- if cookiecutter.need_database == "y" %}
 echo "Yes, Service has database"
 {%- endif %}
