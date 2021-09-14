2ef2e40c18aba1bf54e165a3233b31b81740d3ce
Sahil Gulati <sahil.gulati1991@outlook.com>
2021-09-14 15:11:16 +0530

    Update hello.sh

--- template/hello.sh
+++ template/hello.sh
@@ -4,4 +4,3 @@ echo "Hello World";
 echo "Normal {{ cookiecutter.service_name }}"
 echo "Snake case {{ cookiecutter.service_name_snake_case }}"
 echo "Pascal case {{ cookiecutter.service_name_pascalcase }}"
-echo "Pascal case1 {{ cookiecutter.service_name_pascalcase1 }}"
