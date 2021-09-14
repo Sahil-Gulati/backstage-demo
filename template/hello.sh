ddd9b9fe44b1e8bc1936fe1956d224b760a901bd
Sahil Gulati <sahil.gulati1991@outlook.com>
2021-09-14 15:16:28 +0530

    Update hello.sh

--- template/hello.sh
+++ template/hello.sh
@@ -5,3 +5,6 @@ echo "Normal {{ cookiecutter.service_name }}"
 echo "Snake case {{ cookiecutter.service_name_snake_case }}"
 echo "Pascal case {{ cookiecutter.service_name_pascalcase }}"
 echo "Repo full path {{ cookiecutter.repo_full_path }}"
+{%- if cookiecutter.need_database == "y" %}
+  echo "Database is there..."
+{%- endif %}
